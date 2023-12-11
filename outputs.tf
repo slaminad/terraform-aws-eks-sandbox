ca1e9c2847df072932d127061ff01bb29dc8dabb
Jordan Acosta <jordan@nuon.co>
2023-12-11 13:05:28 -0800

    Ja/125 security group outputs (#16)
    * feat(aws-eks): add security group outputs
    
    * feat(aws-eks-byovpc): add security group outputs

--- outputs.tf
+++ outputs.tf
@@ -8,13 +8,15 @@ output "cluster" {
   // NOTE: these are declared here -
   // https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest?tab=outputs
   value = {
-    arn = module.eks.cluster_arn
+    arn                        = module.eks.cluster_arn
     certificate_authority_data = module.eks.cluster_certificate_authority_data
-    endpoint = module.eks.cluster_endpoint
-    name = module.eks.cluster_name
-    platform_version = module.eks.cluster_platform_version
-    status = module.eks.cluster_status
-    oidc_issuer_url = module.eks.cluster_oidc_issuer_url
+    endpoint                   = module.eks.cluster_endpoint
+    name                       = module.eks.cluster_name
+    platform_version           = module.eks.cluster_platform_version
+    status                     = module.eks.cluster_status
+    oidc_issuer_url            = module.eks.cluster_oidc_issuer_url
+    cluster_security_group_id  = module.eks.cluster_security_group_id
+    node_security_group_id     = module.eks.node_security_group_id
   }
 }
 
@@ -23,21 +25,21 @@ output "vpc" {
   // https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest?tab=outputs
   value = {
     name = module.vpc.name
-    id = module.vpc.vpc_id
+    id   = module.vpc.vpc_id
     cidr = module.vpc.vpc_cidr_block
-    azs = module.vpc.azs
+    azs  = module.vpc.azs
 
     private_subnet_cidr_blocks = module.vpc.private_subnets_cidr_blocks
-    private_subnet_ids = module.vpc.private_subnets
+    private_subnet_ids         = module.vpc.private_subnets
 
     public_subnet_cidr_blocks = module.vpc.public_subnets_cidr_blocks
-    public_subnet_ids = module.vpc.public_subnets
+    public_subnet_ids         = module.vpc.public_subnets
   }
 }
 
 output "account" {
   value = {
-    id = data.aws_caller_identity.current.account_id
+    id     = data.aws_caller_identity.current.account_id
     region = var.region
   }
 }
@@ -45,18 +47,18 @@ output "account" {
 
 output "ecr" {
   value = {
-    repository_url = module.ecr.repository_url
-    repository_arn = module.ecr.repository_arn
+    repository_url  = module.ecr.repository_url
+    repository_arn  = module.ecr.repository_arn
     repository_name = local.vars.id
-    registry_id = module.ecr.repository_registry_id
-    registry_url = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
+    registry_id     = module.ecr.repository_registry_id
+    registry_url    = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com"
   }
 }
 
 output "public_domain" {
   value = {
     nameservers = aws_route53_zone.public.name_servers
-    name = aws_route53_zone.public.name
+    name        = aws_route53_zone.public.name
     zone_id     = aws_route53_zone.public.id
   }
 }
@@ -64,7 +66,7 @@ output "public_domain" {
 output "internal_domain" {
   value = {
     nameservers = aws_route53_zone.internal.name_servers
-    name = aws_route53_zone.internal.name
+    name        = aws_route53_zone.internal.name
     zone_id     = aws_route53_zone.internal.id
   }
 }
