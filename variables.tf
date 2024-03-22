930ce0f2c4f845c0ef73a176381522f94195be16
Jordan Acosta <jordan@nuon.co>
2024-03-22 15:07:56 -0700

    feat(aws-eks): update vars (#53)

--- variables.tf
+++ variables.tf
@@ -1,38 +1,17 @@
 locals {
-  tags = merge({ nuon_id = var.nuon_id }, var.tags)
-
-  /* external_dns = { */
-  /*   registry           = "txt" */
-  /*   provider           = "aws" */
-  /*   policy             = "sync" */
-  /*   triggerLoopOnEvent = true */
-  /*   interval           = "15m" */
-  /* } */
-
-  vars = {
-    id     = var.nuon_id
-    region = var.region
-  }
+  install_name   = (var.install_name != "" ? var.install_name : var.cluster_name)
+  install_region = var.region
+  tags = merge(
+    var.tags,
+    { nuon_id = var.nuon_id },
+    var.additional_tags,
+  )
 }
 
-variable "nuon_id" {
+variable "install_name" {
   type        = string
-  description = "The nuon id for this install. Used for naming purposes."
-}
-
-variable "assume_role_arn" {
-  type        = string
-  description = "The role arn to assume during provisioning of this sandbox."
-}
-
-variable "tags" {
-  type        = map(any)
-  description = "List of custom tags to add to the install resources. Used for taxonomic purposes."
-}
-
-variable "region" {
-  type        = string
-  description = "The region to launch the cluster in"
+  description = "The name of this install. Will be used for the EKS cluster, various tags, and other resources."
+  default     = ""
 }
 
 variable "cluster_name" {
@@ -41,7 +20,7 @@ variable "cluster_name" {
   default     = ""
 }
 
-variable "cluster_version" {
+variable "eks_version" {
   type        = string
   description = "The Kubernetes version to use for the EKS cluster."
   default     = "1.28"
@@ -53,33 +32,55 @@ variable "min_size" {
   description = "The minimum number of nodes in the managed node group."
 }
 
-variable "desired_size" {
-  type        = number
-  default     = 2
-  description = "The desired size."
-}
-
 variable "max_size" {
   type        = number
   default     = 5
   description = "The maximum number of nodes in the managed node group."
 }
 
+variable "desired_size" {
+  type        = number
+  default     = 2
+  description = "The desired number of nodes in the managed node group."
+}
+
 variable "default_instance_type" {
   type        = string
   default     = "t3a.medium"
-  description = "The EC2 instance types to use for the EKS cluster's default node group."
-}
-
-variable "external_access_role_arns" {
-  type        = list(string)
-  description = "Roles for external access to the cluster."
+  description = "The EC2 instance type to use for the EKS cluster's default node group."
 }
 
 variable "admin_access_role_arn" {
   description = "Optional role to provide admin access to the cluster."
   type        = string
   default     = ""
+}
+
+variable "additional_tags" {
+  type        = map(any)
+  description = "Extra tags to append to the default tags that will be added to install resources."
+}
+
+# Automatically set by Nuon when provisioned.
+
+variable "nuon_id" {
+  type        = string
+  description = "The nuon id for this install. Used for naming purposes."
+}
+
+variable "region" {
+  type        = string
+  description = "The region to launch the cluster in"
+}
+
+variable "assume_role_arn" {
+  type        = string
+  description = "The role arn to assume during provisioning of this sandbox."
+}
+
+variable "external_access_role_arns" {
+  type        = list(string)
+  description = "Roles for external access to the cluster."
 }
 
 variable "waypoint_odr_namespace" {
@@ -92,13 +93,18 @@ variable "waypoint_odr_service_account_name" {
   description = "Service account that the ODR iam role should be assumable from."
 }
 
+variable "public_root_domain" {
+  type        = string
+  description = "public root domain."
+}
+
 // NOTE: if you would like to create an internal load balancer, with TLS, you will have to use the public domain.
 variable "internal_root_domain" {
   type        = string
   description = "internal root domain."
 }
 
-variable "public_root_domain" {
-  type        = string
-  description = "public root domain."
+variable "tags" {
+  type        = map(any)
+  description = "List of custom tags to add to the install resources. Used for taxonomic purposes."
 }
